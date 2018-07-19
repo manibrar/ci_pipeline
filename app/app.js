fd2a65f370bc03ec1c8552b2977fe1ecea5dd6c9
manibrar <manvirbrar1988@gmail.com>
2018-07-19 12:18:53 +0100

    correct version

--- app/app.js
+++ app/app.js
@@ -3,19 +3,19 @@ var app = express();
 var exec = require('child_process').exec;
 var mongoose = require('mongoose');
 var Post = require('./models/post');
+var AdvancedMaths = require('./modules/advanced-maths');
 
 app.set('view engine' , 'ejs');
 
 app.use(express.static('public'));
 
 app.get('/' , function(req , res){
-
   res.render("index");
-
 });
 
 // connect to database
 if(process.env.DB_HOST) {
+  
   mongoose.connect(process.env.DB_HOST);
 
   app.get("/posts" , function(req,res){
@@ -29,7 +29,7 @@ if(process.env.DB_HOST) {
 app.get('/fibonacci/:n' , function(req,res){
 
   // high cpu usage function
-  var value = fibonacci(req.params.n);
+  var value = AdvancedMaths.fibonacci(req.params.n);
 
   res.render("fibonacci" , {index:req.params.n, value:value});
 });
@@ -40,23 +40,10 @@ app.get('/fibonacci/:n' , function(req,res){
 //     res.render("hackable/index", {stdout:stdout, command:req.params.command});
 //   });
 // });
-
-app.listen(3000 , function(){
-  console.log('Your app is ready and listening on port 3000');
+var port = process.env.PORT || 3000 ;
+app.listen(port , function(){
+  console.log(`Your app is ready and listening on port ${port}`);
 });
 
-
-// deliberately poorly implemented fibonnaci
-function fibonacci(n) {
-
-  if(n == 0)
-    return 0;
-
-  if(n == 1)
-    return 1;
-
-  return fibonacci(n - 1) + fibonacci(n - 2);
-
-}
 
 module.exports = app;
